22cfa921ac1216572809362c1f13db5e6e29cc6b
David Jelenc <david.jelenc@fri.uni-lj.si>
2019-10-30 09:08:53 +0100

    Adds the initial application

--- app/src/main/java/tetramax/android/MainActivity.java
+++ app/src/main/java/tetramax/android/MainActivity.java
@@ -1,22 +1,19 @@
 package tetramax.android;
 
-import android.app.ActivityManager;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -24,21 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView musicInfoTextView;
     private Button playButton, stopButton, startServiceButton, stopServiceButton;
 
-    private MusicService service;
-
-    private ServiceConnection connection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.i(TAG, "onServiceConnected()");
-            MainActivity.this.service = ((LocalBinder) service).service;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            Log.i(TAG, "onServiceDisconnected()");
-            MainActivity.this.service = null;
-        }
-    };
+    private final MediaPlayer player = new MediaPlayer();
+    private final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,35 +39,31 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (service != null) {
-                    service.play();
-                }
+                play();
             }
         });
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (service != null) {
-                    service.stop();
-                }
+                stop();
             }
         });
         startServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Intent intent = new Intent(MainActivity.this, MusicService.class);
-                startService(intent);
-                bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Start service button will be used in the service implementation.",
+                        Toast.LENGTH_SHORT).show();
             }
         });
         stopServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (service != null) {
-                    unbindService(connection);
-                    service = null;
-                    stopService(new Intent(MainActivity.this, MusicService.class));
-                }
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Stop service button will be used in the service implementation.",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -92,52 +72,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart()");
-
-        if (isServiceRunning()) {
-            bindService(new Intent(MainActivity.this, MusicService.class),
-                    connection, Context.BIND_AUTO_CREATE);
-        }
-
-        // subscribes to receiving messages from service using filter "mplayer"
-        LocalBroadcastManager.getInstance(this).registerReceiver(
-                receiver, new IntentFilter("mplayer"));
     }
-
-    /**
-     * Handles messages coming from the service
-     */
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (musicInfoTextView != null) {
-                musicInfoTextView.setText(intent.getStringExtra("song"));
-            }
-        }
-    };
 
     @Override
     protected void onStop() {
         Log.i(TAG, "onStop()");
-        unbindService(connection);
-        service = null;
-
-        // unregisters from receiving messages from the service
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+        player.release();
         super.onStop();
     }
 
-    private boolean isServiceRunning() {
-        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        if (activityManager == null) {
-            return false;
+    public void play() {
+        if (player.isPlaying()) {
+            return;
         }
-        final List<ActivityManager.RunningServiceInfo> services = activityManager.getRunningServices(Integer.MAX_VALUE);
+        final List<String> files = getFiles();
+        final String song = files.get(random.nextInt(files.size()));
 
-        for (ActivityManager.RunningServiceInfo runningServiceInfo : services) {
-            if (runningServiceInfo.service.getClassName().equals(MusicService.class.getCanonicalName())) {
-                return true;
-            }
+        Log.i(TAG, "Playing song " + song);
+
+        try {
+            final AssetFileDescriptor descriptor = getAssets().openFd(song);
+            player.setDataSource(
+                    descriptor.getFileDescriptor(),
+                    descriptor.getStartOffset(),
+                    descriptor.getLength()
+            );
+            descriptor.close();
+            player.prepare();
+        } catch (IOException e) {
+            Log.w(TAG, "Could not open file", e);
+            return;
         }
-        return false;
+
+        player.setLooping(true);
+        player.start();
+
+        // display song info
+        musicInfoTextView.setText(song);
+    }
+
+    public void stop() {
+        if (player.isPlaying()) {
+            player.stop();
+        }
+
+        player.reset();
+
+        // display song info
+        musicInfoTextView.setText("");
+    }
+
+    /**
+     * Returns the list of mp3 files in the assets folder
+     *
+     * @return
+     */
+    private List<String> getFiles() {
+        final List<String> mp3s = new ArrayList<>();
+        try {
+            final String[] files = getAssets().list("");
+            if (files == null) {
+                return mp3s;
+            }
+
+            for (String fileName : files) {
+                if (fileName.toLowerCase().endsWith("mp3")) {
+                    mp3s.add(fileName);
+                }
+            }
+        } catch (IOException e) {
+            Log.w(TAG, "Error while getting files.", e);
+        }
+
+        return mp3s;
     }
 }
